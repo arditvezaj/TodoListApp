591248dc1aec98dfdcb251c01ad23af8edbaf974
Ardit Vezaj <arditvezaj1@gmail.com>
2023-05-31 22:45:17 +0200

    some amazing styles and colors changes

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   Button,
   FlatList,
+  Pressable,
 } from "react-native";
 
 export default function App() {
@@ -18,9 +19,12 @@ export default function App() {
   };
 
   const addTodoHandler = () => {
+    if (enteredTodo.trim().length === 0) {
+      return;
+    }
     setTodoList((prevTodoList) => [
-      ...prevTodoList,
       { id: Date.now().toString(), text: enteredTodo },
+      ...prevTodoList,
     ]);
     setEnteredTodo("");
   };
@@ -30,18 +34,17 @@ export default function App() {
       <StatusBar style="auto" />
       <View style={styles.container}>
         <Text style={styles.title}>My TODO App</Text>
-        <TextInput
-          placeholder="Enter your todo!"
-          value={enteredTodo}
-          onChangeText={enteredTodoHandler}
-          style={styles.enteredTodo}
-        />
-        <View style={styles.button}>
-          <Button
-            onPress={addTodoHandler}
-            title="Add New Todo"
-            color="tomato"
+        <View style={{ flexDirection: "row" }}>
+          <TextInput
+            placeholder="Enter your todo!"
+            value={enteredTodo}
+            onChangeText={enteredTodoHandler}
+            style={styles.enteredTodo}
           />
+          <Pressable style={styles.button} onPress={addTodoHandler}>
+            <Text style={styles.buttonText}>+</Text>
+          </Pressable>
+          {/* <Button onPress={addTodoHandler} title="Add New Todo" color="white" /> */}
         </View>
         <View style={styles.todoList}>
           <FlatList
@@ -64,38 +67,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 3,
-    backgroundColor: "#fff",
+    backgroundColor: "#713770",
+    color: "white",
     alignItems: "center",
     justifyContent: "center",
   },
   title: {
-    flex: 0.5,
-    fontSize: 23,
+    fontSize: 25,
+    fontWeight: 600,
     marginTop: 60,
-    color: "orange",
+    color: "white",
   },
   enteredTodo: {
-    paddingVertical: 10,
-    paddingHorizontal: 80,
+    padding: 12,
+    marginTop: 30,
+    width: "70%",
     borderRadius: 6,
-    borderWidth: 2,
-    borderColor: "gray",
+    borderColor: "white",
+    backgroundColor: "white",
   },
   singleTodo: {
-    backgroundColor: "red",
-    marginVertical: 4,
+    backgroundColor: "#E95670",
+    marginVertical: 7,
     borderRadius: 6,
+    marginHorizontal: 2,
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
   },
   todoText: {
     color: "white",
-    fontWeight: 500,
+    fontWeight: 600,
     padding: 8,
+    fontSize: 15,
   },
   todoList: {
-    flex: 3,
-    marginTop: 50,
+    flex: 2,
+    margin: 30,
+    width: "90%",
   },
   button: {
-    marginTop: 5,
+    backgroundColor: "#FB8DA0",
+    width: 50,
+    height: 50,
+    borderRadius: "100%",
+    marginLeft: 20,
+    marginTop: 30,
+    fontWeight: 500,
+    fontSize: 23,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: 600,
+    color: "white",
+    marginBottom: 3,
   },
 });
